edc8510ab6700be771d6486c82e46c1ac4aaf09f
rocket12213 <rocket12213@gmail.com>
2019-10-31 22:59:01 +0900

    컬럼 created_at(최초생성), updated_at(수정발생)을 추가하였습니다.

--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class Users(models.Model) :
-	email    = models.CharField(max_length=20)
-	password = models.Charfield(max_length=400)
-
+	email      = models.CharField(max_length=20)
+	password   = models.Charfield(max_length=400)
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
 	class Meta :
-		db_table = 'users'
+            db_table = 'users'
 
 # Create your models here.
