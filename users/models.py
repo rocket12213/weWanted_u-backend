0052523b310b193f26054921d35e7ce5ce8dc9df
rocket12213 <rocket12213@gmail.com>
2019-11-01 14:06:47 +0900

    데이터베이스 컬럼 추가 내역, 마이그레이션 파일 생성 확인함.

--- users/models.py
+++ users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Users(models.Model) :
-	email      = models.CharField(max_length=20)
-	password   = models.Charfield(max_length=400)
-        created_at = models.DateTimeField(auto_now_add=True)
-        updated_at = models.DateTimeField(auto_now=True)
-	class Meta :
-            db_table = 'users'
+    email      = models.CharField(max_length=20)
+    password   = models.CharField(max_length=400)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta :
+        db_table = 'users'
+             
 # Create your models here.
