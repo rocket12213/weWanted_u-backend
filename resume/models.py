b747babcfadec4fae9d7a2ff0d8aa8cb0d4908d8
rocket12213 <rocket12213@gmail.com>
2019-11-04 23:25:57 +0900

    URL필드 최대 길이 수정, 생성일자, 수정일자 클래스의 하단으로 이동

--- resume/models.py
+++ resume/models.py
@@ -6,25 +6,25 @@ class Saving_Types(models.Model) :
     saving_type = models.CharField(max_length=15)
 
 class Resume(models.Model) :
-    created_at   = models.DateTimeField(auto_now_add=True)
-    updated_at   = models.DateTimeField(auto_now=True) 
     saving_type  = models.ForeignKey(Saving_Types, on_delete=models.CASCADE)
     title        = models.CharField(max_length=30) 
     phone        = models.CharField(max_length=10)
     email        = models.CharField(max_length=20)
     blog         = models.URLField(max_length=50)
     about_me     = models.TextField()	
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True) 
 
 class My_Portfolio(models.Model) :
-    created_at  = models.DateTimeField(auto_now_add=True)
-    updated_at  = models.DateTimeField(auto_now=True)
     user	= models.ForeignKey(Users, on_delete=models.CASCADE)
     resume	= models.ForeignKey(Resume, on_delete=models.CASCADE, null=True)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
 
 class Projects(models.Model) : #리스트 요소(한 resume 안에 여러개의 프로 젝트)
     resume        = models.ForeignKey(Resume, on_delete=models.CASCADE)
     project_title = models.CharField(max_length=30)
-    github        = models.URLField(max_length=50)
+    github        = models.URLField(max_length=3000)
     description   = models.TextField()
     what_did_i_do = models.TextField()
     tech_stack    = models.TextField()
