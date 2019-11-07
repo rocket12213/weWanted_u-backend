bd40d02105b13895d03f71e91604d3e1cfcc9b1a
lordmyshepherd <lordmyshepherd29@gmail.com>
2019-11-07 16:39:09 +0900

    add many_to_many_field to user for follow feature

--- users/models.py
+++ users/models.py
@@ -1,12 +1,22 @@
-from django.db import models
+from django.db      import models
+from job.models     import Jobs
 
 class Users(models.Model) :
-    email      = models.CharField(max_length=20)
-    password   = models.CharField(max_length=400)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    email       = models.CharField(max_length=20)
+    password    = models.CharField(max_length=400)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+    job         = models.ManyToManyField(Jobs, through='Follow')
 
     class Meta :
         db_table = 'users'
-             
-# Create your models here.
+
+class Follow(models.Model) :
+    user        = models.ForeignKey(Users, on_delete=models.CASCADE)
+    job         = models.ForeignKey(Jobs, on_delete=models.CASCADE)
+    follow      = models.BooleanField(default=False)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+
+    class Meta :
+        db_table ='follow'
